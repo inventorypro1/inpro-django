60c76bdddd4876362e10169a67ab1d42c7f70675
Marc de Faria <mdefaria@dalet.com>
2021-04-03 15:52:16 +0100

    Add Locker, Item and ItemType model. Add Locker detail class view

--- inpro_django/inpro_home/models.py
+++ inpro_django/inpro_home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Locker(models.Model):
+    name = models.CharField(max_length=100)
+    # TODO: Make this into a django-address field
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Used to store the various types of items that a customer has
+class ItemType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Used to store an instance of an ItemType in a locker
+class Item(models.Model):
+    itemType = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    locker = models.ForeignKey(Locker, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.itemType.name + " item"
+
+    class Meta:
+        unique_together = ('itemType', 'locker')
+
